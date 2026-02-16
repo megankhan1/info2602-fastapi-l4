bd76ca6a3b0e1f6867f4771b981eecd4d57ab18c
megankhan1 <megankhan500@gmail.com>
2026-02-16 16:45:21 -0400

    Update auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlmodel import select
-from app.database import SessionDep, get_session
-from sqlmodel import Session
-from app.models import RegularUser, User
-from app.auth import encrypt_password, verify_password, create_access_token, get_current_user
+from app.database import SessionDep
+from app.models import *
+from app.auth import encrypt_password, verify_password, create_access_token, AuthDep
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 from fastapi import status
@@ -13,23 +12,28 @@ auth_router = APIRouter(tags=["Authentication"])
 @auth_router.post("/token")
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    db: Annotated[Session, Depends(get_session)]
-):
+    db: SessionDep
+) -> Token:
+    # Try logging in a Regular User
     user = db.exec(select(RegularUser).where(RegularUser.username == form_data.username)).one_or_none()
     if not user or not verify_password(plaintext_password=form_data.password, encrypted_password=user.password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        #Try logging in an admin
+        user = db.exec(select(Admin).where(Admin.username == form_data.username)).one_or_none()
+        if not user or not verify_password(plaintext_password=form_data.password, encrypted_password=user.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
         
-    access_token = create_access_token(data={"sub": user.id, "role": user.role},)
+    access_token = create_access_token(data={"sub": user.username, "role": user.role},)
 
-    return {"access_token": access_token, "token_type": "bearer"}
+    return Token(access_token=access_token, token_type="bearer")
 
-@auth_router.get("/identify")
-def get_user_by_id(db: Annotated[Session, Depends(get_session)], user: User = Depends(get_current_user)):
+@auth_router.get("/identify", response_model=UserResponse)
+def get_user_by_id(db: SessionDep, user:AuthDep):
     return user
+
 
 @auth_router.post('/signup', response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def signup_user(user_data: UserCreate, db:SessionDep):
